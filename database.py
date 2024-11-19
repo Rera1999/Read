ee1210d4a0a19a140e6865bca740ae945d7b8ac3
Readh AL- reqe <141080911+Rera1999@users.noreply.github.com>
2024-11-19 23:19:48 +0300

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# إعداد قاعدة البيانات
 engine = create_engine('sqlite:///bot_data.db')
 Base = declarative_base()
 
@@ -13,7 +12,6 @@ class User(Base):
     points = Column(Integer, default=0)
     level = Column(Integer, default=1)
 
-# إنشاء الجداول
 Base.metadata.create_all(engine)
 
 Session = sessionmaker(bind=engine)
